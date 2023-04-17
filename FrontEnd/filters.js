89179e9f1be2a430f39c12f20fe9e20c2dce7c82
tsboudot <tsboudot@gmail.com>
2023-04-17 17:39:14 +0200

    Réaction Btn filtres, tout disparait

--- FrontEnd/filters.js
+++ FrontEnd/filters.js
@@ -1,47 +1,60 @@
+import { genererWorks } from './works.js';
+
+const works = [];
+
 export function filtrerTout() {
-    genererWorks(works);
-  }
-  
-  export function filtrerObjets() {
-    const worksFiltre = works.filter(function (work) {
-      return work.category.name === "objets";
+  genererWorks(works);
+}
+
+export function filtrerObjets() {
+  const worksFiltre = works.filter(function (work) {
+    return work.category.name === "objets";
+  });
+  genererWorks(worksFiltre);
+}
+
+export function filtrerAppartements() {
+  const worksFiltre = works.filter(function (work) {
+    return work.category.name === "appartements";
+  });
+  genererWorks(worksFiltre);
+}
+
+export function filtrerHotelsEtRestaurants() {
+  const worksFiltre = works.filter(function (work) {
+    return work.category.name === "Hotels & restaurants";
+  });
+  genererWorks(worksFiltre);
+}
+
+export function genererFiltres() {
+  const portfolioSection = document.getElementById('portfolio');
+  const filtersContainer = document.createElement('div');
+  filtersContainer.id = 'filters';
+  portfolioSection.appendChild(filtersContainer);
+
+  const filterButtons = [
+    { name: 'Tous', id: 'all', action: filtrerTout },
+    { name: 'objets', id: 'objets', action: filtrerObjets },
+    { name: 'Appartements', id: 'appartements', action: filtrerAppartements },
+    { name: 'Hotels et Restaurants', id: 'hotels-et-restaurants', action: filtrerHotelsEtRestaurants }
+  ];
+
+  filterButtons.forEach(button => {
+    const filterButton = document.createElement('button');
+    filterButton.textContent = button.name;
+    filterButton.id = button.id;
+    filtersContainer.appendChild(filterButton);
+
+    button.element = filterButton;
+
+    filterButton.addEventListener('click', () => {
+      filterButtons.forEach(button => {
+        button.element.classList.remove('active');
+      });
+
+      filterButton.classList.add('active');
+      button.action();
     });
-    genererWorks(worksFiltre);
-  }
-  
-  export function filtrerAppartements() {
-    const worksFiltre = works.filter(function (work) {
-      return work.category.name === "appartements";
-    });
-    genererWorks(worksFiltre);
-  }
-  
-  export function filtrerHotelsEtRestaurants() {
-    const worksFiltre = works.filter(function (work) {
-      return work.category.name === "Hotels & restaurants";
-    });
-    genererWorks(worksFiltre);
-  }
-  
-  export function genererFiltres() {
-    const portfolioSection = document.getElementById('portfolio');
-    const filtersContainer = document.createElement('div');
-    filtersContainer.id = 'filters';
-    portfolioSection.appendChild(filtersContainer);
-  
-    const filterButtons = [
-      { name: 'Tous', id: 'all', action: filtrerTout },
-      { name: 'objets', id: 'objets', action: filtrerObjets },
-      { name: 'Appartements', id: 'appartements', action: filtrerAppartements },
-      { name: 'Hotels et Restaurants', id: 'hotels-et-restaurants', action: filtrerHotelsEtRestaurants }
-    ];
-  
-    filterButtons.forEach(button => {
-      const filterButton = document.createElement('button');
-      filterButton.textContent = button.name;
-      filterButton.id = button.id;
-      filtersContainer.appendChild(filterButton);
-  
-      button.element = filterButton;
-    });
-  }
+  });
+}
